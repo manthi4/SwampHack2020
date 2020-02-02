9abc00d7a5af6b4b265e8bec28caca3a164fa0b9
manthi4 <44146639+manthi4@users.noreply.github.com>
2020-02-02 11:20:22 -0500

    added predictor

--- predictor.py
+++ predictor.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  2 10:43:12 2020
+Created on Sun Feb  2 11:17:04 2020
 
 @author: manthi
 """
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -16,20 +17,10 @@ from sklearn.pipeline import make_pipeline
 from numpy import savetxt
 
 data = pd.read_csv('path')
-
-######Dividing the data
-X, y = data.iloc[:,0:4].values,  data.iloc[:,4].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#Normalising all the data
-scale = StandardScaler()
-X_train = scale.fit_transform(X_train)
-X_test  = scale.fit_transform(X_test)
+weights = pd.read_csv('weights.csv')
 
 M = 3
 poly_model = make_pipeline(PolynomialFeatures(M),LinearRegression())
 
-yfit = poly_model.poly.fit_transform(data);
-
-w = poly_model.weights;
-savetxt('weights.csv', w, delimiter=',')
-
+predictions = poly_model.predict(data);
+savetxt('predictions.csv', predictions, delimiter=',')
